0e40978937efa23a8f69e6dbcadc38a68f7453d4
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-17 09:37:18 -0700

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone: true
 })
 export class AppComponent {
   title: string = 'DLOR';
