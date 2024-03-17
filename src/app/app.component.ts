243e4762c6a1f0632906be248c5d91767509d697
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-17 09:36:00 -0700

    d

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true
+})
+export class AppComponent {
+  title: string = 'DLOR';
+  menu: string[] = ["Home", "Shop", "Contact"];
+  isOpen: boolean = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+    const hamburger = document.querySelector('.hamburger-menu');
+    hamburger?.classList.toggle('open');
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
