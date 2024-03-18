32c46dffdaf7de24d112d3df1225a9d4673ee148
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-18 14:20:36 -0700

    animation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +13,24 @@ export class AppComponent {
   title: string = 'DLOR';
   menu: string[] = ["Home", "Shop", "Contact"];
   isOpen: boolean = false;
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
 
+  openMenu() {
+    this.renderer?.addClass(this.document.body, 'menu-open');
+  }
+  
+  closeMenu() {
+    this.renderer?.removeClass(this.document.body, 'menu-open');
+  }
   toggleMenu() {
     this.isOpen = !(this.isOpen);
-    console.log(this.isOpen);
-    const hamburger = document.querySelector('.hamburger-menu');
-    hamburger?.classList.toggle('open');
+  console.log(this.isOpen);
+  const hamburger = document.querySelector('.hamburger-menu');
+  hamburger?.classList.toggle('open');
+  if (this.isOpen) {
+    this.openMenu();
+  } else {
+    this.closeMenu();
+  }
   }
 }
