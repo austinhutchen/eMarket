80a606775cae3439b649986491ecf172723061c7
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-18 14:38:27 -0700

    small patch (likely css, check release notes)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,24 +13,33 @@ export class AppComponent {
   title: string = 'DLOR';
   menu: string[] = ["Home", "Shop", "Contact"];
   isOpen: boolean = false;
-  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {
+    this.document.addEventListener('scroll', () => {
+      if (this.document.documentElement.scrollTop === 0) {
+        this.renderer.addClass(this.document.body, 'black-background');
+      } else {
+        this.renderer.removeClass(this.document.body, 'black-background');
+      }
+    });
+
+  }
 
   openMenu() {
     this.renderer?.addClass(this.document.body, 'menu-open');
   }
-  
+
   closeMenu() {
     this.renderer?.removeClass(this.document.body, 'menu-open');
   }
   toggleMenu() {
     this.isOpen = !(this.isOpen);
-  console.log(this.isOpen);
-  const hamburger = document.querySelector('.hamburger-menu');
-  hamburger?.classList.toggle('open');
-  if (this.isOpen) {
-    this.openMenu();
-  } else {
-    this.closeMenu();
-  }
+    console.log(this.isOpen);
+    const hamburger = document.querySelector('.hamburger-menu');
+    hamburger?.classList.toggle('open');
+    if (this.isOpen) {
+      this.openMenu();
+    } else {
+      this.closeMenu();
+    }
   }
 }
