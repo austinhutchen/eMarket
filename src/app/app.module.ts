b05f19d1ba8a57606f7d252681ae1647fdfde80c
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-18 16:41:55 -0700

    small patch (likely css, check release notes)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgFor } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+// Import your components here
+
+import { AppComponent } from './app.component';
+
+// Define your routes here
+export const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'store', component: undefined },
+  { path: 'horde', component: undefined },
+  // other routes...
+];
 
 @NgModule({
-
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes), // Add this
   ],
   providers: [],
 })
---
+++ src/app/horde/horde.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horde',
+  standalone: true,
+  imports: [],
+  templateUrl: './horde.component.html',
+  styleUrl: './horde.component.css'
+})
+export class HordeComponent {
+
+}
