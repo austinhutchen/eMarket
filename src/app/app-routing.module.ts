aae7b67d7ee09e2eccda90920d6e9375aa383f8f
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-18 21:06:46 -0700

    small patch (likely css, check release notes)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { HordeComponent } from './horde/horde.component';
 
-
+// Define your routes
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+{ path: '', component: AppComponent },
+{ path: 'horde', component: HordeComponent },
+{ path: 'store', component: StoreComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
