6e6202f69518197e7c5c0e5b98e8fc652376f31e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-04-08 08:16:01 -0700

    small patch (likely css, check release notes)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
@@ -8,24 +8,33 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent  {
   title: string = 'DLOR';
   overLay: boolean = false;
   menu: string[] = ["Home", "Shop", "Contact"];
   isOpen: boolean = false;
+  @ViewChild('musicIcons') musicIcons!: ElementRef;
   isScrolled: boolean = false;
+  isDesktop: boolean = false;
   inputValue: string = '';
 
   submitForm() {
     console.log(this.inputValue);
   }
   constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {
+    this.renderer.listen('window', 'resize', (e: Event) => {
+      this.isDesktop = window.innerWidth > 768;
+    });
     this.renderer.listen('window', 'scroll', (e: Event) => {
       if (window.scrollY > 0) {
         this.isScrolled = true;
       } else {
         this.isScrolled = false;
       }
+    });
+    this.renderer.listen('window', 'load', (e: Event) => {
+      this.isDesktop = window.innerWidth > 768;
+    (this.isDesktop)? this.renderer.removeClass(this.musicIcons.nativeElement, 'd-none'): this.renderer.addClass(this.musicIcons.nativeElement, 'd-none');
     });
   }
   toggleOverlay() {
@@ -34,13 +43,6 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('background') backgroundImage!: ElementRef;
 
 
-  ngAfterViewInit() {
-    this.renderer.listen('window', 'load', () => {
-      console.log("loaded");
-
-    });
-
-  }
   openMenu(): void {
     this.renderer?.addClass(this.document.body, 'menu-open');
     this.toggleOverlay();
