0c534caedc098612ce5722290556220c25675956
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-04-08 10:11:47 -0700

    small patch (likely css, check release notes)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { StoreComponent } from './store/store.component'; // Import StoreComponent
 import { HordeComponent } from './horde/horde.component'; // Import HordeComponent
+import { StoreComponent } from './store/store.component'; // Import StoreComponent
 
 export const routes: Routes = [
-  { path: 'home', component: AppComponent },
-  { path: 'store', component: StoreComponent }, // Use StoreComponent
-  { path: 'horde', component: HordeComponent }, // Use HordeComponent
+  { path: 'horde', component: HordeComponent },
+  { path: 'store', component: StoreComponent },
+  // other routes...
 ];
 
 @NgModule({
