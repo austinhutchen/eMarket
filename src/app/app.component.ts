5f4292fa09740f9044ab7ab82db6c04b33facc50
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-04-13 14:52:59 -0700

    REMOVED ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,20 @@ import { Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone:true
+  standalone: true
 })
 
 export class AppComponent implements AfterViewInit {
   public title: string = 'DLOR';
   private overLay: boolean = false;
-  private header!: HTMLElement | null | undefined;
-  private hamburgerMenu!: HTMLElement | null | undefined;
-  private mailBox!: HTMLElement | null | undefined;
+  private header!: Element | null | undefined;
+  private hamburgerMenu!: HTMLElement | null;
+  private mailBox!: HTMLElement | null;
   private menu: string[] = ["Home", "Shop", "Contact"];
   private isOpen: boolean = false;
   public isScrolled: boolean = false;
@@ -25,7 +26,7 @@ export class AppComponent implements AfterViewInit {
   constructor(
     private renderer: Renderer2,
     private el: ElementRef,
-    @Inject(DOCUMENT) private document: Document | null,
+    @Inject(DOCUMENT) private document: Document,
     @Inject(PLATFORM_ID) private platformId: Object | null
   ) {
 
@@ -44,24 +45,31 @@ export class AppComponent implements AfterViewInit {
 
 
   ngAfterViewInit() {
+
     this.header = this.document?.querySelector('.header');
     this.hamburgerMenu = this.document?.querySelector('.hamburger-menu');
     this.mailBox = this.document?.querySelector('.mailBox');
     if (!this.header || !this.hamburgerMenu || !this.mailBox || !this.platformId) {
       return;
     }
-    if (isPlatformBrowser(this.platformId) && 'IntersectionObserver' in window) {
-      this.observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (this.mailBox && entry.isIntersecting) {
-            this.renderer.addClass(this.mailBox, 'fade-in');
-          } else if (this.mailBox) {
-            this.renderer.removeClass(this.mailBox, 'fade-in');
-          }
-        });
-      }, {
-        threshold: 1.0 // Adjust this value
-      });
+    if (typeof window !== 'undefined' && window.IntersectionObserver) {
+      if (isPlatformBrowser(this.platformId)) {
+        if ('IntersectionObserver' in window) {
+          this.observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+              if (this.mailBox && entry.isIntersecting) {
+                this.renderer.addClass(this.mailBox, 'fade-in');
+              } else if (this.mailBox) {
+                this.renderer.removeClass(this.mailBox, 'fade-in');
+              }
+            });
+          }, {
+            threshold: 1.0 // Adjust this value
+          });
+        }
+      } else {
+        console.log('Running on the server, skipping IntersectionObserver code');
+      }
     }
   }
 
