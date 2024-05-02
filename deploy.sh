33c9ef4aa1f2e346caeabd842d729e25abf9fcc0
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-05-02 08:23:59 -0600

    small patch (likely css, check release notes)

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-{ git add -A; git commit -m "small patch (likely css, check release notes)"; git push origin HEAD:main --force; }
+{ pnpm run build; pnpm run deploy; vercel build; git add -A; git commit -m "small patch (likely css, check release notes)"; git push origin HEAD:main --force; }
